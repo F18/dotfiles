188560704ce0522fed2163e76e8495a1c05274dd
Ramy Rashad <ra.rashad@gmail.com>
2023-10-18 05:48:33 -0400

    Add ASUS G40 laptop config for W11

--- installLinks.sh
+++ installLinks.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo " "
-echo -n "Pick a system from (case sensitive) options: laptop, mac, oddjob, guillimin, scinet, westgrid, solarship: "
+echo -n "Pick a system from (case sensitive) options: asusg40, laptop, mac, oddjob, guillimin, scinet, westgrid, solarship: "
 read -e LOCALSYS
 
 DIRBKP=~/dotfiles/backups/.dotfiles-bkp-$(date +%Y%m%d.%H%M)
 
-mkdir $DIRBKP
+mkdir -p $DIRBKP
 
 cp ~/.bashrc $DIRBKP
 cp ~/.bashrc-global $DIRBKP
@@ -29,9 +29,9 @@ rm -f ~/.tmux.conf
 rm -f ~/.vimrc
 rm -f ~/.screenrc 
 rm -f ~/.ackrc 
-rm ~/.i3/config
-rm ~/.i3/i3status.conf
-rm ~/.ipython/profile_default/ipython_config.py
+#rm ~/.i3/config
+#rm ~/.i3/i3status.conf
+#rm ~/.ipython/profile_default/ipython_config.py
 
 ln -s ~/dotfiles/bashrc-$LOCALSYS ~/.bashrc
 ln -s ~/dotfiles/bashrc-global ~/.bashrc-global
@@ -41,6 +41,6 @@ ln -s ~/dotfiles/viper ~/.viper
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/screenrc ~/.screenrc
 ln -s ~/dotfiles/ackrc ~/.ackrc
-ln -s ~/dotfiles/i3config ~/.i3/config
-ln -s ~/dotfiles/i3status.conf ~/.i3/i3status.conf
-ln -s ~/dotfiles/ipython_config.py ~/.ipython/profile_default/ipyton_config.py
+#ln -s ~/dotfiles/i3config ~/.i3/config
+#ln -s ~/dotfiles/i3status.conf ~/.i3/i3status.conf
+#ln -s ~/dotfiles/ipython_config.py ~/.ipython/profile_default/ipyton_config.py
