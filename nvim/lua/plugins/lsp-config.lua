044c69e49bc537dabe0417b7a04267901aec4624
Ramy Rashad <ra.rashad@gmail.com>
2024-01-31 06:12:46 -0500

    Add autocompletion and snippet plugins and config

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -20,13 +20,23 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
+
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
             local lspconfig = require("lspconfig")
 
             -- Setup language servers.
-            lspconfig.lua_ls.setup({})
-            lspconfig.pyright.setup{}
-            lspconfig.tsserver.setup {}
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.pyright.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.tsserver.setup ({
+                capabilities = capabilities,
+            })
             lspconfig.rust_analyzer.setup {
+                capabilities = capabilities,
                 -- Server-specific settings. See `:help lspconfig-setup`
                 settings = {
                     ['rust-analyzer'] = {},
